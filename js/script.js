bb6b9943e0c3e9f6192b5ba1d0db5fe25388da9c
Murilo Armondes <murilo.armondes.758@gmail.com>
2026-09-22 14:10:23 -0300

    Projeto Conexao Solidaria

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const cpf = document.getElementById("cpf");
+const telefone = document.getElementById("telefone");
+const cep = document.getElementById("cep");
+const formulario = document.getElementById("formCadastro");
+
+
+/* MÁSCARA DO CPF */
+
+cpf.addEventListener("input", function () {
+
+    let valor = cpf.value.replace(/\D/g, "");
+
+    valor = valor.slice(0, 11);
+
+    valor = valor.replace(
+        /(\d{3})(\d)/,
+        "$1.$2"
+    );
+
+    valor = valor.replace(
+        /(\d{3})(\d)/,
+        "$1.$2"
+    );
+
+    valor = valor.replace(
+        /(\d{3})(\d{1,2})$/,
+        "$1-$2"
+    );
+
+    cpf.value = valor;
+});
+
+
+/* MÁSCARA DO TELEFONE */
+
+telefone.addEventListener("input", function () {
+
+    let valor = telefone.value.replace(/\D/g, "");
+
+    valor = valor.slice(0, 11);
+
+    valor = valor.replace(
+        /^(\d{2})(\d)/g,
+        "($1) $2"
+    );
+
+    valor = valor.replace(
+        /(\d{5})(\d{4})$/,
+        "$1-$2"
+    );
+
+    telefone.value = valor;
+});
+
+
+/* MÁSCARA DO CEP */
+
+cep.addEventListener("input", function () {
+
+    let valor = cep.value.replace(/\D/g, "");
+
+    valor = valor.slice(0, 8);
+
+    valor = valor.replace(
+        /(\d{5})(\d)/,
+        "$1-$2"
+    );
+
+    cep.value = valor;
+});
+
+
+/* ENVIO DO FORMULÁRIO */
+
+formulario.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    if (formulario.checkValidity()) {
+
+        alert(
+            "Cadastro realizado com sucesso! Obrigado por fazer parte da Conexão Solidária."
+        );
+
+        formulario.reset();
+    }
+});
